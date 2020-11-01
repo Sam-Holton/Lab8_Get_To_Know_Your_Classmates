f0764803de7babfaec3f454e8ec8ceb6a7b9842e
Sam <samuel.holton@gmail.com>
2020-11-01 16:29:45 -0500

    Added loops and methods

--- Lab8_Get_To_Know_Your_Classmates/Program.cs
+++ Lab8_Get_To_Know_Your_Classmates/Program.cs
@@ -7,10 +7,21 @@ namespace Lab8_Get_To_Know_Your_Classmates
     {
         static void Main(string[] args)
         {
-            
-            int student = StudentQuery();
-            IdentifyAndDetailQuery(student);
-            
+            bool loop = true;
+            string[] classmateName = { "Sam", "Kate", "Phoenix", "Zach", "Linda", "Garry", "Joshua", "Ben", "Katie", "Andrika", "Gregory", "Cheryl", "Paul", "Janelle", "Jennifer", "BJ", "John", "Laura", "Roger", "Jamie" };
+            string[] hometown = { "Jackson, MI", "Bacon Level, AL", "Deadhorse, AK", "Nothing, AZ", "Greasy Corner, AR", "Forks of Salmon, CA", "Nowhere, CO", "Hazardville, CT", "Blue Ball, DE", "Fluffy Landing, FL", "Ty Ty, GA", "Volcano, HI", "Santa, ID", "Bone Gap, IL", "Young America, IN", "What Cheer, IA", "Nuetral, KS", "Hell for Certain, KY", "Book, LA", "Friendship, ME" };
+            string[] favoriteFood = { "Rice Pudding", "Chorizo", "Gouda Holland", "Ravioli", "Gnocchi", "Durian", "Brie de Meaux", "Miso Soup", "Sarma", "Pizza Margherita", "Pizza Napoletana", "Satay", "Banchan", "Pavlova", "Eclair", "Grilled Cheese", "Kombu", "Nori", "Parfait", "Mooncake" };
+
+            do
+            {
+                int student = StudentQuery();
+                IdentifyAndDetailQuery(student, classmateName, hometown, favoriteFood);
+                Console.Write("Would you like to know more? (enter \"yes\" or \"no\"):");
+                string userDecision = Console.ReadLine();
+                if (userDecision == "yes") Console.Clear();             
+                else if (userDecision == "no") loop = false;
+            } while (loop);
+
         }
 
         static int StudentQuery()
@@ -28,13 +39,38 @@ namespace Lab8_Get_To_Know_Your_Classmates
             } while (true);
         }
 
-        static void IdentifyAndDetailQuery(int student)
+        static void IdentifyAndDetailQuery(int student, string[] classmateName, string[] hometown, string[] favoriteFood)
         {
-            string[] classmateName = { "Sam", "Kate", "Phoenix", "Zach", "Linda", "Garry", "Joshua", "Ben", "Katie", "Andrika", "Gregory", "Cheryl", "Paul", "Janelle", "Jennifer", "BJ", "John", "Laura", "Roger", "Jamie" };
-            string[] hometown = { "Jackson, MI", "Bacon Level, AL", "Deadhorse, AK", "Nothing, AZ", "Greasy Corner, AR", "Forks of Salmon, CA", "Nowhere, CO", "Hazardville, CT", "Blue Ball, DE", "Fluffy Landing, FL", "Ty Ty, GA", "Volcano, HI", "Santa, ID", "Bone Gap, IL", "Young America, IN", "What Cheer, IA", "Nuetral, KS", "Hell for Certain, KY", "Book, LA", "Friendship, ME" };
-            string[] favoriteFood = { "Rice Pudding", "Chorizo", "Gouda Holland", "Ravioli", "Gnocchi", "Durian", "Brie de Meaux", "Miso Soup", "Sarma", "Pizza Margherita", "Pizza Napoletana", "Satay", "Banchan", "Pavlova", "Eclair", "Grilled Cheese", "Kombu", "Nori", "Parfait", "Mooncake" };
-            
-            Console.WriteLine($"Student {student} is {classmateName[student-1]}. What would you like to know about him/her? ");
+            bool loop = false;
+            Console.Write($"Student {student} is {classmateName[student - 1]}. What would you like to know about him/her? (enter \"hometown\" or \"favorite food\") ");
+
+            do
+            {
+                
+                string userChoice = Console.ReadLine();
+                           
+                switch (userChoice)
+                {
+                    case "hometown":
+                        {
+                            Console.WriteLine($"{classmateName[student - 1]} is from {hometown[student - 1]}");
+                            loop = false;
+                            break;
+                        }
+                    case "favorite food":
+                        {
+                            Console.WriteLine($"{classmateName[student - 1]} enjoys eating {favoriteFood[student - 1]}");
+                            loop = false;
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Write("That data does not exist. Please try again. (enter \"hometown\" or \"favorite food\") ");
+                            loop = true;
+                            break;
+                        }
+                }
+            } while (loop);
         }
     }
 }
